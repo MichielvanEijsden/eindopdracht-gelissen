910b7862b0cff4bf43b74d53396a5eb6684bee12
MichielvanEijsden <143597805+MichielvanEijsden@users.noreply.github.com>
2023-12-30 15:21:50 +0100

    added webshop needs more tuning and styling

--- src/pages/webshop/Webshop.jsx
+++ src/pages/webshop/Webshop.jsx
@@ -1,5 +1,46 @@
+import './Webshop.css'
+import {useEffect, useState} from "react";
+import axios from "axios";
+
 function Webshop(){
+    const [allItems,setAllItems]=useState([])
+    const [error,setError]=useState()
+
+    useEffect(() => {
+        fetchAllItems()
+    }, []);
+    async function fetchAllItems() {
+        try {
+            const result = await axios.get('https://fakestoreapi.com/products')
+            setAllItems(result.data)
+            console.log(result.data)
+        } catch (e) {
+            setError(e)
+            console.error(error);
+        }
+    }
+
     return(
-        <h1>Webshop</h1>)
+        <>
+            <div className='outer-container'>
+                <div className='inner-container'>
+            <section className='center-page-container'>
+            <h2>welcome to the Webshop</h2>
+            <ul>
+                {allItems.map((items) =>{
+                    return(
+                        <li key={items.id}>
+                            <h3>{items.title}</h3>
+                            <img src={items.image} alt={items.title}/>
+                            <p>{items.description}</p>
+                        </li>
+                    )
+                })}
+            </ul>
+            </section>
+                </div>
+        </div>
+        </>
+        )
 }
 export default Webshop
