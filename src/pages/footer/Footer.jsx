7f4e343234b9964966e8fcdaf240214887f2df71
MichielvanEijsden <143597805+MichielvanEijsden@users.noreply.github.com>
2024-01-02 22:03:11 +0100

    schaling aangepast voor mobile en test footer gemaakt

--- src/pages/footer/Footer.jsx
+++ src/pages/footer/Footer.jsx
@@ -1,5 +1,18 @@
-
+import './Footer.css'
+import fb_logo from '../../assets/facebook.svg'
 function Footer(){
-    return(<p>footer text</p>)
+    return(
+        <footer>
+            <div className='outer-container'>
+            <div className='inner-container'>
+                <div className='footer-bar'>
+                    <img src={fb_logo}  alt='fb logo'/>
+                    dit word een hele mooie footer ooit
+                </div>
+
+            </div>
+            </div>
+        </footer>
+    )
 }
 export default Footer
