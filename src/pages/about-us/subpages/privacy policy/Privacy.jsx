1b9d4e063dc02640fee97d2d1a81a70136ad1be1
MichielvanEijsden <143597805+MichielvanEijsden@users.noreply.github.com>
2024-01-05 19:12:07 +0100

    privacy page styled, navigation remade, dropdown added

--- src/pages/about-us/subpages/privacy policy/Privacy.jsx
+++ src/pages/about-us/subpages/privacy policy/Privacy.jsx
@@ -3,37 +3,56 @@ import './Privacy.css';
 function Privacy() {
     return (
         <>
-            Wie wij zijn
+            <div className='outer-container'>
+                <div className='inner-container'>
+                    <div className='center-page-container'>
+                        <div className='privacy-wrapper'>
+            <h2>Wie wij zijn</h2>
 
-            Ons site adres is: https://www.gelissentweewielers.nl.
-            Welke gegevens wij gebruiken
+            <p>Ons site adres is: https://www.gelissentweewielers.nl</p>
 
-            Naam
-            Adres
-            Postcode
-            E-mailadres
-            Telefoonnummer
-            Bankrekeningnummer
+            <h2>Welke gegevens wij gebruiken</h2>
+            <ul className='privacy-list'>
+                <li>Naam</li>
+                <li>Adres</li>
+                <li>Postcode</li>
+                <li>E-mailadres</li>
+                <li>Telefoonnummer</li>
+                <li>Bankrekeningnummer</li>
+            </ul>
+            <h2>Ingesloten inhoud van andere sites</h2>
 
-            Ingesloten inhoud van andere sites
+            <p>Berichten op deze site kunnen ingesloten inhoud bevatten (bijvoorbeeld video’s, afbeeldingen, enz.).
+                Ingesloten inhoud van andere sites gedraagt zich exact hetzelfde alsof de bezoeker deze andere site
+                heeft bezocht.</p>
 
-            Berichten op deze site kunnen ingesloten inhoud bevatten (bijvoorbeeld video’s, afbeeldingen, enz.). Ingesloten inhoud van andere sites gedraagt zich exact hetzelfde alsof de bezoeker deze andere site heeft bezocht.
+            <p>Deze sites kunnen gegevens over je verzamelen, cookies gebruiken, extra tracking van derde partijen
+                insluiten en je interactie met deze ingesloten inhoud monitoren, inclusief het vastleggen van de
+                interactie met ingesloten inhoud als je een account hebt en ingelogd bent op die site.</p>
+            <h2>Met wie we je gegevens delen</h2>
 
-            Deze sites kunnen gegevens over je verzamelen, cookies gebruiken, extra tracking van derde partijen insluiten en je interactie met deze ingesloten inhoud monitoren, inclusief het vastleggen van de interactie met ingesloten inhoud als je een account hebt en ingelogd bent op die site.
-            Met wie we je gegevens delen
+            <p>Jouw gegevens worden niet voor marketingdoeleinden gebruikt. Gegevens worden alleen gedeeld als wij daar
+                een wettelijke plicht voor hebben.</p>
+            <h2>Hoelang we je gegevens bewaren</h2>
 
-            Jouw gegevens worden niet voor marketingdoeleinden gebruikt. Gegevens worden alleen gedeeld als wij daar een wettelijke plicht voor hebben.
-            Hoelang we je gegevens bewaren
+            <p>De gegevens worden zolang bewaard als wettelijk is bepaald. Gegevens m.b.t. financiële zaken, zoals gegevens
+            die op een factuur (naam, adres e.d.) of die op onze bankrekeningafschriften (bankrekeningnummer) staan,
+                worden zoals wettelijke bepaald 7 jaar bewaard.</p>
 
-            De gegevens worden zolang bewaard als wettelijk is bepaald. Gegevens m.b.t. financiële zaken, zoals gegevens die op een factuur (naam, adres e.d.) of die op onze bankrekeningafschriften (bankrekeningnummer) staan, worden zoals wettelijke bepaald 7 jaar bewaard.
-
-            Videobeelden van ons bewakingssysteem worden maximaal 4 weken bewaard, tenzij er een incident heeft plaatsgevonden. In geval van een incident zullen de videobeelden bewaard worden zolang nodig is in verband met onderzoek door de politie.
-            Welke rechten je hebt over je gegevens
-
-            Als je klant bij ons bent mag je om een informatieverzoek in te dienen.
-            Je mag ook een correctieverzoek indienen als je gegevens niet meer kloppen.
-            Je kan ook verzoeken dat we alle persoonlijke gegevens die we van je hebben wissen. Dit bevat geen gegevens die we verplicht moeten bewaren in verband met administratieve, wettelijke of beveiligingsdoeleinden.
-
+            <p>Videobeelden van ons bewakingssysteem worden maximaal 4 weken bewaard, tenzij er een incident heeft
+            plaatsgevonden. In geval van een incident zullen de videobeelden bewaard worden zolang nodig is in verband
+            met onderzoek door de politie.</p>
+            <h2>elke rechten je hebt over je gegevens</h2>
+<ul className='privacy-list'>
+            <li>Als je klant bij ons bent mag je om een informatieverzoek in te dienen.</li>
+            <li>Je mag ook een correctieverzoek indienen als je gegevens niet meer kloppen.</li>
+            <li>Je kan ook verzoeken dat we alle persoonlijke gegevens die we van je hebben wissen. Dit bevat geen gegevens
+                die we verplicht moeten bewaren in verband met administratieve, wettelijke of beveiligingsdoeleinden.</li>
+</ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
